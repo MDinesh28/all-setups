7fe163d763470225d1fc2901db31edd17781f5d7
DINESH MEKA <mekadinesh28@gmail.com>
2024-11-02 19:40:20 +0530

    Update jenkins.sh

--- jenkins.sh
+++ jenkins.sh
@@ -1,15 +1,31 @@
-#STEP-1: INSTALLING GIT JAVA-1.8.0 MAVEN 
-yum install git java-1.8.0-openjdk maven -y
+# Update system packages
+sudo yum update -y
 
-#STEP-2: GETTING THE REPO (jenkins.io --> download -- > redhat)
+# Install Git, OpenJDK 8, and Maven
+sudo yum install -y git java-1.8.0-openjdk maven
+
+# Add Jenkins repository
 sudo wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
 sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
 
-#STEP-3: DOWNLOAD JAVA11 AND JENKINS
-amazon-linux-extras install java-openjdk11 -y
-yum install jenkins -y
-update-alternatives --config java
+# Update all packages (including new repo packages)
+sudo yum upgrade -y
 
-#STEP-4: RESTARTING JENKINS (when we download service it will on stopped state)
-systemctl start jenkins.service
-systemctl status jenkins.service
+# Add Amazon Corretto 17 repository
+sudo curl -L -o /etc/yum.repos.d/corretto.repo https://yum.corretto.aws/corretto.repo
+
+# Install Amazon Corretto 17
+sudo yum install -y java-17-amazon-corretto
+
+# Set the default Java version to Amazon Corretto 17
+sudo alternatives --config java
+
+# Install Jenkins
+sudo yum install -y jenkins
+
+# Enable and start Jenkins service
+sudo systemctl enable jenkins
+sudo systemctl start jenkins
+
+# Check Jenkins service status
+sudo systemctl status jenkins
